68c67321f8d6eec53566f2771d0a3ae27cb7f999
Ali Malik <amali28@uwo.ca>
2018-11-15 18:05:28 +0000

    Finished tutorial

--- angular-demo1/src/app/courses/courses.component.ts
+++ angular-demo1/src/app/courses/courses.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from '../courses.service'
+import {AutoGrowDirective} from '../auto-grow.directive'
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css'],
-  providers: [CoursesService]
+  providers: [CoursesService, AutoGrowDirective]
 })
 export class CoursesComponent implements OnInit {
   
